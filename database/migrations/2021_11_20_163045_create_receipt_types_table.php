6864f564505312f381f06e1827b1b2c3955c01bd
hauja47 <hoangletrunghau01@gmail.com>
2021-12-07 21:37:05 +0700

    Add column 'slug'

--- database/migrations/2021_11_20_163045_create_receipt_types_table.php
+++ database/migrations/2021_11_20_163045_create_receipt_types_table.php
@@ -15,7 +15,8 @@ class CreateReceiptTypesTable extends Migration
     {
         Schema::create('receipt_types', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
