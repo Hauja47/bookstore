bba38edb8fd5fabc66bf6a08b446a3a4fbf34224
huyvn799 <quochuy2001.kb@gmail.com>
2021-12-13 23:24:05 +0700

    Update DatabaseSeeder.php
    Add seeder for employee

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\Employee::create([
+            'full_name' => 'Nguyễn Văn A',
+            'phone_number' => '0354621452',
+            'email' => 'anv@gmail.com',
+            'address' => 'somewhere',
+            'salary' => '800000',
+            'user_id' => \App\Models\User::create([
+                'username' => 'test',
+                'password' => '123456',
+                'role' => '1'
+            ])->id
+        ]);
     }
 }
