bf58cc1e5e7da7e777026796ea844f1c612a2c3e
hauja47 <hoangletrunghau01@gmail.com>
2021-12-18 00:23:26 +0700

    Finish Controller

--- app/Http/Controllers/ReceiptController.php
+++ app/Http/Controllers/ReceiptController.php
@@ -3,7 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Receipt;
+use App\Models\Customer;
+use App\Models\Employee;
+use App\Models\Invoice;
+use App\Models\Parameter;
+use App\Models\Provider;
 use Illuminate\Http\Request;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class ReceiptController extends Controller
 {
@@ -38,8 +44,84 @@ class ReceiptController extends Controller
     public function store(Request $request)
     {
         request()->validate([
-            'money' => 'required|numeric',
+            'giver_type' => 'required',
+            'note' => 'required',
         ]);
+
+        if (request('giver_customer_id') == null && request('giver_provider_id') == null && request('giver_employee_id') == null)
+        {
+            Alert::error('Tên đối tượng không được bỏ trống!');
+            return back();
+        }
+
+        if (request('invoice_id') && request('giver_type') == 'Khách hàng')
+        {
+            if (!($invoice = Invoice::find(request('invoice_id'))))
+            {
+                Alert::error('Hóa đơn HD'.request('invoice_id').' không tồn tại');
+                return back();
+            }
+
+            if ($invoice->balance < request('money'))
+            {
+                Alert::error('Số tiền khách hàng nộp nhiều hơn số tiền còn lại của hóa đơn');
+                return back();
+            }
+
+            if (Customer::find(request('giver_customer_id'))->debt - request('money') > Parameter::find(3)->value)
+            {
+                Alert::error('Số tiền nợ sau khi nộp của khách hàng lớn hơn số tiền nợ tối đa');
+                return back();
+            }
+        }
+
+        if (request('giver_type') == 'Khách hàng')
+        {
+            $customer = Customer::find(request('giver_customer_id'));
+            $receipt = $customer->receipts()->create([
+                'employee_id' => auth()->user()->employee->id,
+                'money' => request('money'),
+                'note' => $invoice ? request('note').'. Phiếu thu tiền còn lại cho hóa đơn HD'.$invoice->id : request('note')
+            ]);
+
+            if (request('invoice_id'))
+            {
+                $receipt->update([
+                    'invoice_id' => request('invoice_id')
+                ]);
+
+                $customer->update([
+                    'debt' => $customer->debt - request('money')
+                ]);
+
+                $invoice->update([
+                    'paid' => $invoice->paid + request('money'),
+                    'balance' => $invoice->balance - request('money')
+                ]);
+            }
+        }
+        else if (request('giver_type') == 'Nhân viên')
+        {
+            $receipt = Employee::find(request('giver_employee_id'))->receiptGiver()->create([
+                'employee_id' => auth()->user()->employee->id,
+                'money' => request('money'),
+                'note' => request('note'),
+            ]);
+        }
+        else if (request('giver_type') == 'Nhà cung cấp')
+        {
+            $receipt = Provider::find(request('giver_provider_id'))->receipts()->create([
+                'employee_id' => auth()->user()->employee->id,
+                'money' => request('money'),
+                'note' => request('note')
+            ]);
+        }
+
+        if ($receipt)
+        {
+            return redirect(route('receipts.index'))->withSuccess('Tạo phiếu thu thành công');
+        }
+        return redirect(route('receipts.index'))->withErrors('Tạo phiếu thu thất bại');
     }
 
     /**
@@ -76,7 +158,46 @@ class ReceiptController extends Controller
      */
     public function update(Request $request, Receipt $receipt)
     {
-        //
+        request()->validate([
+            'money' => 'required',
+            'text' => 'required',
+        ]);
+
+        if ($receipt->invoice_id)
+        {
+            $customer = $receipt->giver;
+
+            if ($customer->debt + $receipt->money < request('money'))
+            {
+                Alert::error('Số tiền nợ sau khi thay đổi của khách hàng nhỏ hơn số tiền nộp');
+                return back();
+            }
+
+            if ($customer->debt + $receipt->money - request('money') > Parameter::find(3)->value)
+            {
+                Alert::error('Số tiền nợ sau khi thay đổi của khách hàng lớn hơn số tiền nợ tối đa');
+                return back();
+            }
+
+            $customer->update([
+                'debt' => $customer->debt + $receipt->money - request('money')
+            ]);
+
+            $invoice = $receipt->invoice;
+            $invoice->update([
+                'paid' => $invoice->paid - $receipt->money + request('money'),
+                'balance' => $invoice->balance + $receipt->money - request('money')
+            ]);
+        }
+
+        if ($receipt->update([
+            'money' => request('money'),
+            'note' => request('text')
+        ]))
+        {
+            return redirect(route('receipts.index'))->withSuccess('Thay đổi phiếu thu thành công');
+        }
+        return redirect(route('receipts.index'))->withErrors('Thay đổi phiếu thu thất bại');
     }
 
     /**
@@ -87,6 +208,28 @@ class ReceiptController extends Controller
      */
     public function destroy(Receipt $receipt)
     {
-        //
+        if ($receipt->invoice_id)
+        {
+            $customer = $receipt->giver;
+
+            if ($customer->debt + $receipt->money > Parameter::find(3)->value)
+            {
+                Alert::error('Số tiền nợ sau khi xóa của khách hàng lớn hơn số tiền nợ tối đa');
+                return back();
+            }
+
+            $customer->update([
+                'debt' => $customer->debt + $receipt->money
+            ]);
+
+            $invoice = $receipt->invoice;
+            $invoice->update([
+                'paid' => $invoice->paid - $receipt->money,
+                'balance' => $invoice->balance + $receipt->money
+            ]);
+        }
+
+        $receipt->delete();
+        return back()->withSuccess('Xóa phiếu thu thành công');
     }
 }
